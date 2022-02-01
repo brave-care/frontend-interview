fb8635651b2f72966ad60c473a20efd5aa1a54b3
Kim Niedermaier <kim@bravecare.com>
2022-02-01 10:47:55 -0800

    adds a test for the new time slot component

---
+++ frontend/components/TimeSlot/TimeSlot.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { TimeSlot } from '../TimeSlot';
+
+describe('TimeSlot', () => {
+  it('renders a time slot', () => {
+    const startTime = '2022-02-01T10:00:00.000-08:00';
+    const endTime = '2022-02-01T10:30:00.000-08:00';
+
+    render(<TimeSlot endTime={endTime} startTime={startTime} />);
+
+    const button = screen.getByRole('button', {
+      name: /10:00 AM - 10:30 AM/i,
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+});
