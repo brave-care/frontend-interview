f9b9cd9347ea3fe1f75b98c25c37b37af43524ab
Kim Niedermaier <kim@bravecare.com>
2022-02-01 11:17:39 -0800

    makes time slot a functional component explicit

--- frontend/components/TimeSlot/TimeSlot.tsx
+++ frontend/components/TimeSlot/TimeSlot.tsx
@@ -1,13 +1,11 @@
 import { DateTime } from 'luxon';
+import { FC } from 'react';
 import styles from '../../styles/TimeSlot.module.css';
 
-export const TimeSlot = ({
-  endTime,
-  startTime,
-}: {
+export const TimeSlot: FC<{
   endTime: string;
   startTime: string;
-}) => {
+}> = ({ endTime, startTime }) => {
   const formattedStartTime = DateTime.fromISO(startTime).toFormat('hh:mm a');
   const formattedEndTime = DateTime.fromISO(endTime).toFormat('hh:mm a');
 
