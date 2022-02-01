9df65e1b77530eabee6555df619458412eca9bbe
Kim Niedermaier <kim@bravecare.com>
2022-02-01 12:57:37 -0800

    Create appointment slots for the next 2 months

--- backend/src/appointments.ts
+++ backend/src/appointments.ts
@@ -8,30 +8,42 @@ interface Appointment {
 }
 
 export async function getAppointments(): Promise<Appointment[]> {
-  const openTime = DateTime.now()
-    .setZone('America/Los_Angeles')
-    .startOf('day')
-    .plus({ hours: 10 });
-  const closeTime = openTime.plus({ hours: 10 });
-  const openInterval = Interval.fromDateTimes(openTime, closeTime);
+  // Start with today
+  const now = DateTime.now().setZone('America/Los_Angeles').startOf('day');
 
-  const clinic1Appointments: Appointment[] = openInterval
-    .splitBy({ minutes: 30 })
-    .map((i) => ({
-      startTime: i.start.toISO(),
-      endTime: i.end.toISO(),
-      length: i.length('minutes'),
-      clinicId: '1',
-    }));
+  // Get each day for the next 2 months
+  const dates = Interval.fromDateTimes(now, now.plus({ months: 2 })).splitBy({
+    days: 1,
+  });
 
-  const clinic2Appointments: Appointment[] = openInterval
-    .splitBy({ minutes: 30 })
-    .map((i) => ({
-      startTime: i.start.toISO(),
-      endTime: i.end.toISO(),
-      length: i.length('minutes'),
-      clinicId: '2',
-    }));
+  // For each day in the next 2 months, return an interval of an available open times (9am to 9pm)
+  const clinicOpenIntervals = dates.map((interval) => {
+    const start = interval.start.set({ hour: 9, minute: 0 }).startOf('minute');
+    const end = interval.start.set({ hour: 21, minute: 0 }).startOf('minute');
+    return Interval.fromDateTimes(start, end);
+  });
 
-  return [...clinic1Appointments, ...clinic2Appointments];
+  // We'll have 3 clinics (see the /clinics endpoint for specific mocked data per clinic)
+  const clinicIds = [1, 2, 3];
+
+  // For each clinic, return a single array of available appointment slots during the open intervals
+  const appointments = clinicIds.flatMap((id) => {
+    const appointmentsPerOpenIntervals = clinicOpenIntervals.flatMap(
+      (openInterval) => {
+        const appointmentTimeSlots = openInterval.splitBy({ minutes: 30 });
+        const appointment = appointmentTimeSlots.map((slot) => ({
+          startTime: slot.start.toISO(),
+          endTime: slot.end.toISO(),
+          length: slot.length('minutes'),
+          clinicId: `${id}`,
+        }));
+
+        return appointment;
+      }
+    );
+
+    return appointmentsPerOpenIntervals;
+  });
+
+  return appointments;
 }
