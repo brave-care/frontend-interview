412b66cd03c207b66fa1c4908c8117aee42f18e1
Kim Niedermaier <kim@bravecare.com>
2022-02-16 15:22:20 -0800

    places mocked appt data inside of test

--- frontend/components/Appointments/Appointments.test.tsx
+++ frontend/components/Appointments/Appointments.test.tsx
@@ -1,6 +1,26 @@
 import { render, screen } from '@testing-library/react';
-import { appointments } from '../../__mocks__/appointments';
 import { Appointments } from '../Appointments';
+
+const appointments = [
+  {
+    startTime: '2022-02-01T10:00:00.000-08:00',
+    endTime: '2022-02-01T10:30:00.000-08:00',
+    length: 30,
+    clinicId: '1',
+  },
+  {
+    startTime: '2022-02-01T10:30:00.000-08:00',
+    endTime: '2022-02-01T11:00:00.000-08:00',
+    length: 30,
+    clinicId: '1',
+  },
+  {
+    startTime: '2022-02-01T11:00:00.000-08:00',
+    endTime: '2022-02-01T11:30:00.000-08:00',
+    length: 30,
+    clinicId: '1',
+  },
+];
 
 describe('Appointments', () => {
   it('renders a title', () => {
